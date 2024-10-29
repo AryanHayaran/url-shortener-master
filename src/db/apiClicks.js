e8557a5041b772e949d80334f8702c87fc767826
Aryan Hayaran <aryanhayaran5@gmail.com>
2024-10-30 00:48:49 +0530

    drg

--- src/db/apiClicks.js
+++ src/db/apiClicks.js
@@ -1,16 +1,16 @@
 import {UAParser} from "ua-parser-js";
 import supabase from "./supabase";
 
-// export async function getClicks() {
-//   let {data, error} = await supabase.from("clicks").select("*");
+export async function getClicks() {
+  let {data, error} = await supabase.from("clicks").select("*");
 
-//   if (error) {
-//     console.error(error);
-//     throw new Error("Unable to load Stats");
-//   }
+  if (error) {
+    console.error(error);
+    throw new Error("Unable to load Stats");
+  }
 
-//   return data;
-// }
+  return data;
+}
 
 export async function getClicksForUrls(urlIds) {
   const {data, error} = await supabase
--- src/db/supabase.js
+++ src/db/supabase.js
@@ -4,6 +4,11 @@ export const supabaseUrl = import.meta.env.VITE_SUPABASE_URL;
 const supabaseKey = import.meta.env.VITE_SUPABASE_KEY;
 const supabase = createClient(supabaseUrl, supabaseKey);
 
+export const corsHeaders = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'authorization, x-client-info, apikey, content-type',
+}
+
 export default supabase;
 
 // import { createClient } from '@supabase/supabase-js';
